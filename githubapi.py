a74d8e1c235014ec440733bc8b7b19fe08f669a9
giannamiggins <gmiggins@stevens.edu>
2020-09-29 12:24:54 -0400

    testing 2 users

--- githubapi.py
+++ githubapi.py
@@ -4,9 +4,9 @@ import unittest
 #gianna miggins
 #SSW 567 Homework 4a 
 
-def getRepos():
+def getRepos(user):
     #input github username
-    user = 'giannamiggins'
+    #user = 'giannamiggins'
     #input("Enter a github username")
     print("searching", user,"'s repositories...")
 
@@ -49,7 +49,10 @@ def getRepos():
 #when commits are over 30, it defaults to 30
 class TestGitRepos(unittest.TestCase):
     def testGiannaRepo(self):
-        self.assertEqual(getRepos(), {'FocusTheHedgehog': 30, 'GEDCOM_Project': 30, 'helloworld': 1, 'magicdeck': 30, 'twitterAPI': 2})
+        self.assertEqual(getRepos('giannamiggins'), {'FocusTheHedgehog': 30, 'GEDCOM_Project': 30, 'githubapi567': 8, 'helloworld': 1, 'magicdeck': 30, 'twitterAPI': 2})
+    def testJasonRepo(self):
+        self.assertEqual(getRepos('richkempinski'), {'csp': 2, 'hellogitworld': 30, 'helloworld': 6, 'Mocks': 10, 'Project1': 2, 'richkempinski.github.io': 9, 'threads-of-life': 1, 'try_nbdev': 2, 'try_nbdev2': 5})
+
 
 if __name__ == '__main__':
 
